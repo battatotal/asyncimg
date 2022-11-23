d561813995c2d07be2f29db46e8d09f39b25eb67
battatotal <91632132+battatotal@users.noreply.github.com>
2022-11-23 17:44:41 +0300

    Create asyncimg.py

---
+++ asyncimg.py
@@ -0,0 +1,35 @@
+#Асинхронный загрузчик файлов
+import asyncio
+import aiohttp
+from time import time
+
+def write_image(data):
+    filename = f"file-{int(time() * 1000)}.jpeg"
+    with open (filename, "wb") as file:
+        file.write(data)
+
+
+async def fetch_content(url, session):
+    async with session.get(url, allow_redirects=True) as response:
+        data = await response.read()
+        write_image(data)
+
+
+async def main():
+    url = "https://loremflickr.com/320/240"
+    tasks = []
+
+    async with aiohttp.ClientSession() as session:
+        for i in range(10):
+            task = asyncio.create_task(fetch_content(url, session))
+            tasks.append(task)
+
+
+        await asyncio.gather(*tasks)
+
+
+if __name__ == '__main__':
+    t0 = time()
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy()) # эта строка только для Windows
+    asyncio.run(main())
+    print(time() - t0)
